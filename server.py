11ad69f40dbd1b4f1e233ee5d5f0c0a92de0950c
PaulHancock <paul.hancock@curtin.edu.au>
2017-06-02 15:02:07 +0800

    working test server

---
+++ server.py
@@ -0,0 +1,30 @@
+#! python
+import random
+import string
+
+import cherrypy
+
+__author__ = 'Paul Hancock'
+__date__ = ''
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+          <head></head>
+          <body>
+            <form method="get" action="generate">
+              <input type="text" value="8" name="length" />
+              <button type="submit">Give it now!</button>
+            </form>
+          </body>
+        </html>"""
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        return ''.join(random.sample(string.hexdigits, int(length)))
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
+
